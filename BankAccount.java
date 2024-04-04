12bee3e153b4dcd6cb7b290dfce78b70489da261
jamiharsha <145543653+jamiharsha@users.noreply.github.com>
2024-04-04 12:03:49 +0530

    Task 3 java programming

---
+++ BankAccount.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
